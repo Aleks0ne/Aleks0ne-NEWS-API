a655cef3ad0e08fba6d440a6c434e6de10fd1435
Aleksei <Mr.Puzako@gmail.com>
2022-07-05 01:05:02 +0300

    add: apiKey

--- migration-newip-to-ts/news-JS-main/src/components/controller/appLoader.js
+++ migration-newip-to-ts/news-JS-main/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e1536853551245008bf7901259fe3ef3', // получите свой ключ https://newsapi.org/
         });
     }
 }
